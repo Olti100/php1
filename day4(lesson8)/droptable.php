7cb68db587425455038056c29b477d217f7a75a4
Olti100 <oltiqerimi10@gmail.com>
2025-09-27 14:42:37 +0200

    day4

---
+++ day4(lesson8)/droptable.php
@@ -0,0 +1,15 @@
+<?php
+
+try{
+    $pdo=new PDO("mysql:host=localhost;dbname=db","root","");
+    $sql="DROP TABLE users";
+
+    $pdo->exec($sql);
+
+    echo "Table dropped";
+}catch(PDOException $e){
+    echo $e.getMessage();
+}
+
+
+?>
