114675e865517ad5684d1ad73932a0e30d3f242b
Olti100 <oltiqerimi10@gmail.com>
2025-10-04 14:40:52 +0200

    lesson9

---
+++ day5(lesson9)/index.php
@@ -0,0 +1,31 @@
+<?php
+// $username=$_GET['username'];
+// $password=$_GET['password'];
+
+// echo $username;
+// echo "<br>";
+// echo $password;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="add.php" method="POST">
+        <!-- <label for="username">Username:</label>
+        <input type="text" name="username" id="username" placeholder="username"><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" placeholder="password"><br>
+        <input type="submit" name="" id="" value="Submit"> -->
+        <input type="text" name="name" id=""><br>
+        <input type="text" name="username" id=""><br>
+        <input type="email" name="email" id=""><br>
+        <button type="submit" name="submit">Add</button>
+    </form>
+</body>
+</html>
